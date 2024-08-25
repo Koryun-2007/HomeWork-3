92644d47e3e37adf2c779f0de49708f875145781
Koryun-2007 <koryunavetisyan3.@gmail.com>
2024-08-25 17:46:31 +0400

    add Task 1

---
+++ HomeWork-3.js
@@ -0,0 +1,17 @@
+// function findLargest(array){
+//     let bigNum = array[1]
+//     for(let i = 0; i <array.length; i++){
+//         if(bigNum < array[i]){
+//             bigNum = array[i]
+//         }
+//     }
+//     return bigNum
+// }
+// console.log(findLargest([1, 3, 5, 7, 9])); // Output: 9);
+
+
+
+// countVowels("Hello World"); // Output: 3
+// countVowels("JavaScript"); // Output: 3
+// countVowels("OpenAI"); // Output: 3
+
